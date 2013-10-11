b37f058f7dc3704df11872e34b7f8c7c0894db88
Dave Ackerman <dave@moduscreate.com>
2013-10-11 16:12:32 -0400

    test

--- README.md
+++ README.md
@@ -5,16 +5,7 @@ http://dribbble.com/shots/1254439--GIF-Mobile-Form-Interaction?list=users
 
 ![Dribble GIF](http://dribbble.s3.amazonaws.com/users/6410/screenshots/1254439/form-animation-_gif_.gif)
 
-
-
 ## Usage
-
-1. `bower install --save angular-masonry`
-2. Include dependencies in your HTML.
-3. Use the `better-placeholder` directive in place of `placeholder`.
-
-## Example
-
 
 ## TODO
 - tests?
--- src/angular-better-placeholder.js
+++ src/angular-better-placeholder.js
@@ -1,36 +1,35 @@
 'use strict';
 
-angular.module('PlaceholderDemoApp')
-  .directive('betterPlaceholder', function() {
-    return {
-      restrict: 'C',
-      require: 'ngModel',
-      link: function(scope, element, attrs, ngModel) {
-        var origPlaceholderText = attrs.placeholder;
-        var placeholder = '<span class=\"help-block better-placeholder-text\">' + attrs.placeholder + '</span>';
+.directive('betterPlaceholder', function() {
+  return {
+    restrict: 'C',
+    require: 'ngModel',
+    link: function(scope, element, attrs, ngModel) {
+      var origPlaceholderText = attrs.placeholder;
+      var placeholder = '<span class=\"help-block better-placeholder-text\">' + attrs.placeholder + '</span>';
 
-        // watch our model for changes
-        scope.$watch(attrs.ngModel, function(newValue, oldValue) {
-          if (newValue === oldValue) {
-            return;
-          }
-          else if (newValue == "") {
-            element.attr('placeholder', '');
-          }
-          else if (!element.hasClass('better-placeholder-active')) {
-            element.before(placeholder)
-            element.addClass('better-placeholder-active');
-          }
-        });
+      // watch our model for changes
+      scope.$watch(attrs.ngModel, function(newValue, oldValue) {
+        if (newValue === oldValue) {
+          return;
+        }
+        else if (newValue == "") {
+          element.attr('placeholder', '');
+        }
+        else if (!element.hasClass('better-placeholder-active')) {
+          element.before(placeholder)
+          element.addClass('better-placeholder-active');
+        }
+      });
 
-        // remove the placeholder, and set the default placeholde to blank since we already have one
-        element.on('blur', function() {
-          if (element.val() == "") {
-            element.prev().remove();
-            element.removeClass('better-placeholder-active');
-            element.attr('placeholder', origPlaceholderText);
-          }
-        });
-      }
+      // remove the placeholder, and set the default placeholde to blank since we already have one
+      element.on('blur', function() {
+        if (element.val() == "") {
+          element.prev().remove();
+          element.removeClass('better-placeholder-active');
+          element.attr('placeholder', origPlaceholderText);
+        }
+      });
     }
-  })
+  }
+})
