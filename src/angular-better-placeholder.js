68044305387286a868c08d6ff91da1117d64dd91
SimeonC <Simeon@lume.co.nz>
2014-08-22 12:01:41 +1200

    Fixed Some issues with different input types.
    Supports use of the ngList directive.
    Added the styl and compiled CSS that I've been using which should go some way to doing #2.

--- src/angular-better-placeholder.js
+++ src/angular-better-placeholder.js
@@ -2,61 +2,78 @@ angular.module('angularBetterPlaceholder', []).directive('betterPlaceholder', fu
   return {
     restrict: 'C',
     require: '?ngModel',
-    scope: {},
     link: function(scope, element, attrs, ngModel) {
-      var activate, deactivate, isEmpty, placeholder;
+      var activate, deactivate, isEmpty, placeholder, required, _placeholder;
       isEmpty = function(value) {
         if (value == null) {
           value = ngModel && (ngModel != null) ? ngModel.$viewValue : element.val();
         }
-        return !element[0].validity.badInput && (!value || value === '');
+        return !element[0].validity.badInput && !((value != null) && (!angular.isString(value) || value !== '') && (!angular.isArray(value) || !value.isEmpty()) && (angular.isString(value) || angular.isArray(value) || (element.val().trim().length > 0 || !isNaN(value))));
       };
       if (attrs.ngPlaceholder != null) {
-        scope.placeholder = scope.$parent.$eval(attrs.ngPlaceholder);
-        element.attr('placeholder', scope.placeholder);
-        scope.$parent.$watch(attrs.ngPlaceholder, function(val) {
+        _placeholder = attrs.ngPlaceholder;
+        element.attr('placeholder', _placeholder);
+        attrs.$observe('ngPlaceholder', function(val) {
           element.attr('placeholder', val);
-          return placeholder.html(val);
+          placeholder.html(val);
+          if (attrs.required) {
+            return placeholder.append(required);
+          }
         });
       } else if ((attrs.placeholder != null) && attrs.placeholder !== '') {
-        scope.placeholder = attrs.placeholder;
+        _placeholder = attrs.placeholder;
       } else {
         throw "better-placeholder requires an ng-placeholder or placeholder attribute";
       }
-      placeholder = angular.element("<span class='help-block better-placeholder-text'>" + scope.placeholder + "</span>");
+      required = angular.element('<i class="fa fa-asterisk text-danger fa-required"></i>');
+      placeholder = angular.element("<span class='help-block better-placeholder-text'>" + _placeholder + "</span>");
       element.after(placeholder);
       placeholder.on('click', function() {
         return element[0].focus();
       });
+      attrs.$observe('required', function(val) {
+        if (val) {
+          return placeholder.append(required);
+        } else {
+          return required.remove();
+        }
+      });
       activate = function() {
         element.addClass('better-placeholder-active');
-        return placeholder.addClass('active');
+        placeholder.addClass('active');
+        if ((element.previous() != null) && element.previous().hasClass('input-group-btn')) {
+          return element.previous().addClass('better-placeholder-button-active');
+        }
       };
       deactivate = function() {
-        if (isEmpty()) {
-          element.removeClass('better-placeholder-active');
-          return placeholder.removeClass('active');
+        element.removeClass('better-placeholder-active');
+        placeholder.removeClass('active');
+        if ((element.previous() != null) && element.previous().hasClass('input-group-btn')) {
+          return element.previous().removeClass('better-placeholder-button-active');
         }
       };
       element.on('focus', activate);
-      element.on('blur', deactivate);
-      element.on('change', function() {
+      element.on('blur', function() {
         if (isEmpty()) {
-          element.removeClass('better-placeholder-active');
-          return placeholder.removeClass('active');
-        } else {
-          element.addClass('better-placeholder-active');
-          return placeholder.addClass('active');
+          return deactivate();
         }
       });
+      element.on('change', function() {
+        if (isEmpty()) {
+          return deactivate();
+        } else {
+          return activate();
+        }
+      });
+      if ((element.previous() != null) && element.previous().hasClass('input-group-btn')) {
+        element.previous().addClass('better-placeholder-button');
+      }
       if (ngModel != null) {
         return ngModel.$formatters.push(function(value) {
           if (isEmpty(value)) {
-            element.removeClass('better-placeholder-active');
-            placeholder.removeClass('active');
+            deactivate();
           } else {
-            element.addClass('better-placeholder-active');
-            placeholder.addClass('active');
+            activate();
           }
           return value;
         });
